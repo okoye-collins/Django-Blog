5042f39a12cee75f0db1fbaf63b121b74e6f5d3a
okoye-collins <collins.kcj@gmail.com>
2022-07-15 12:42:52 +0100

    config the database to postgresql

--- core/settings.py
+++ core/settings.py
@@ -81,8 +81,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myblog',
+        'USER': 'myblogadmin',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'POST': '5432',
+        
     }
 }
 
